1533bccee006bde7660d0842220974e950b84d9c
DmitryKosmich <kosmich27@mail.ru>
2014-07-08 13:50:57 +0300

    Fixed server configurations.

--- app.js
+++ app.js
@@ -58,6 +58,11 @@ https.createServer(options, app).listen(8000, function() {
     console.log('Express server listening on port ' + '8000');
 });
 */
-http.createServer(app).listen(8000, function(){
-    console.log('Express server listening on port ' + 8000);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+var server = http.createServer(app);
+
+server.listen(server_port, server_ip_address, function () {
+    console.log( "Listening on " + server_ip_address + ", port " + server_port );
 });
