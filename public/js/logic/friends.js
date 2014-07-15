efe8affaf18fe4aed639ed5e3e708b373210db2c
DmitryKosmich <kosmich27@mail.ru>
2014-07-15 16:13:50 +0300

    Fixed friend list.

--- public/js/logic/friends.js
+++ public/js/logic/friends.js
@@ -13,16 +13,14 @@ function showFriends(data){
     var surname = '';
     var homeCity = '';
     if(data.response.friends.count > 0){
-        for(var i = data.response.friends.count-1; i>= 0; i--){
-            if(data.response.friends.items[i].relationship=='friend'){
-                for(var j = data.response.friends.items.length-1; j>= 0; j--){
-                    id = data.response.friends.items[j].id;
-                    name = data.response.friends.items[j].firstName;
-                    surname = data.response.friends.items[j].lastName;
-                    homeCity = data.response.friends.items[j].homeCity;
+        for(var j = data.response.friends.count-1; j>= 0; j--){
+            if(data.response.friends.items[j].relationship=='friend'){
+                id = data.response.friends.items[j].id;
+                name = data.response.friends.items[j].firstName;
+                surname = data.response.friends.items[j].lastName;
+                homeCity = data.response.friends.items[j].homeCity;
 
-                    showFriend(id, name, surname, homeCity);
-                }
+                showFriend(id, name, surname, homeCity);
             }
         }
     }
