d70bbabc9b86985f91b52dc3218ffafb478f999e
DmitryKosmich <kosmich27@mail.ru>
2014-08-08 11:36:19 +0300

    Fixed popup, localization, friend map

--- public/js/logic/services/popup.js
+++ public/js/logic/services/popup.js
@@ -3,7 +3,7 @@
 (function(){
     $( ".reveal-modal-bg_country_popup").click( function(e) {
         if (e.target.id === "bg_country_popup"){
-            closeCountryDialog();
+            countryPopUpHide();
         }
     });
 })();
